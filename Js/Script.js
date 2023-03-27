d52bc8a8e08e93590dd45b0958b13d64794c30b7
Connor Kerrigan <ac111484@avcol.school.nz>
2023-03-27 21:06:18 +1300

    TODO: Navbar consistent across site footer on each page fill all navbar links with pages

--- Js/Script.js
+++ Js/Script.js
@@ -8,33 +8,3 @@ window.addEventListener("scroll", function(){
     
 });
 
-$(document).ready(function(){
-    // Activate Carousel
-    $("#riseValueCarousel").carousel();
-      
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-      $("#riseValueCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-      $("#riseValueCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-      $("#riseValueCarousel").carousel(2);
-    });
-    $(".item4").click(function(){
-      $("#riseValueCarousel").carousel(3);
-    });
-      
-    // Enable Carousel Controls
-    $(".left").click(function(){
-      $("#riseValueCarousel").carousel("prev");
-    });
-    $(".right").click(function(){
-      $("#riseValueCarousel").carousel("next");
-    });
-  
-    $("#riseValueCarousel").on('slide.bs.carousel', function (){
-      alert('A new slide is about to be shown!');
-    });
-  });
