c6df3ec17567a9503369ea63928ca313bf946c03
Connor Kerrigan <ac111484@avcol.school.nz>
2023-03-31 10:11:00 +1300

    Layout functionality to automate navbar and footer updates

--- Js/Script.js
+++ Js/Script.js
@@ -1,10 +1,32 @@
-window.addEventListener("scroll", function(){
+
+
+window.addEventListener("scroll", function () {
     var offset = this.window.pageYOffset;
     var section = this.document.getElementsByClassName("parallaxSection")
-    for(var i in section){
+    for (var i in section) {
         var sectionHeight = section[i].offsetHeight;
         section[i].style.backgroundPositionY = (offset / sectionHeight * -100) + "%";
     }
-    
+
 });
 
+var loadLayoutTop = function () {
+    $.ajax("LayoutTop.html", {
+        success: function (response) {
+            $("#TopContent").html(response);
+        }
+    });
+};
+
+
+$.ajax("LayoutTop.html", {
+    success: function (response) {
+        $("#TopContent").html(response);
+    }
+});
+
+$.ajax("Footer.html", {
+    success: function (response) {
+        $("#Footer").html(response);
+    }
+}); 
