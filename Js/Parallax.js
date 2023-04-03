75cc0a2943b79468d918f71eba43b608a9535fde
Connor Kerrigan <ac111484@avcol.school.nz>
2023-04-03 15:19:51 +1200

    LAYOUT UP AND RUNNING

---
+++ Js/Parallax.js
@@ -0,0 +1,11 @@
+
+
+window.addEventListener("scroll", function () {
+    var offset = this.window.pageYOffset;
+    var section = this.document.getElementsByClassName("parallaxSection")
+    for (var i in section) {
+        var sectionHeight = section[i].offsetHeight;
+        section[i].style.backgroundPositionY = (offset / sectionHeight * -100) + "%";
+    }
+
+});
